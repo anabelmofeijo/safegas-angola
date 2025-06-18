e8ef6f7629eb4e01f14800d91fdb12a3e58b4b4a
Anabelmo Feijó <anabelmofeijo@gmail.com>
2025-06-18 17:30:02 +0100

    Third Commit

--- app/routers/control_router.py
+++ app/routers/control_router.py
@@ -1,7 +1,24 @@
 from app import  APIRouter
+from app.services.control_service import ControlDatabase
+from app.schemas.control_schema import (
+    ControlOff,
+    ControlOn
+)
 
 control = APIRouter()
 
 @control.get("/")
 async def running():
     return {"message":"Control is running"}
+
+@control.post("/on/")
+async def control_on(data: ControlOn):
+    service = ControlDatabase()
+    response = service.ControlAdd(data)
+    return response
+
+@control.post("/off/")
+async def control_off(data: ControlOff):
+    service = ControlDatabase()
+    response = service.ControlAdd()
+    return response
--- app/routers/reading_router.py
+++ app/routers/reading_router.py
@@ -1,7 +1,18 @@
 from app import APIRouter
+from app.services.reading_service import ReadingService
+from app.schemas.reading_schema import (
+    ReadingCreate,
+    ReadingResponse
+)
 
 reading = APIRouter()
 
 @reading.get("/")
 async def running():
     return {"message":"Reading is running"}
+
+@reading.post("/post/")
+async def add_reading(data: ReadingCreate):
+    service = ReadingService()
+    response = service.ReadingAdd(data)
+    return response
