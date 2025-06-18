14d6c4878aab00b37f7e68c84991721eb3842346
Anabelmo Feijó <anabelmofeijo@gmail.com>
2025-06-18 16:43:55 +0100

    Second Commit

--- app/routers/alert_router.py
+++ app/routers/alert_router.py
@@ -1,4 +1,5 @@
 from app import APIRouter
+from app.services.alert_service import AlertService
 
 
 alert = APIRouter()
@@ -7,3 +8,13 @@ alert = APIRouter()
 @alert.get("/")
 async def running():
     return {"message":"alert is running"}
+
+@alert.get("/list/")
+async def create():
+    service = AlertService()
+    response = service.AlertListAll()
+    return response
+
+@alert.post("/post/")
+async def post_alerts():
+    pass
