962f386c0f69fd6354914f631ca094226d45d655
Anabelmo Feijó <anabelmofeijo@gmail.com>
2025-06-18 17:37:30 +0100

    Fouth Commit

--- app/routers/reading_router.py
+++ app/routers/reading_router.py
@@ -16,3 +16,21 @@ async def add_reading(data: ReadingCreate):
     service = ReadingService()
     response = service.ReadingAdd(data)
     return response
+
+@reading.get("/list/")
+async def reading_list():
+    service = ReadingService()
+    response = service.ReadingList()
+    return response
+
+@reading.get("/{id}")
+async def find(id: int):
+    service = ReadingService()
+    response = service.ReadingById(id=id)
+    return response
+
+@reading.get("/user/{id}")
+async def reading_user_by_id(id: int):
+    service = ReadingService()
+    response = service.ReadingUserById(user_id=id)
+    return response
